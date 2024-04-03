4064568cf9504177fc0729794ee3206aaf9305f1
shanto-sa <shantostatussa@gmail.com>
2024-04-04 01:21:58 +0600

    react api data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setPosts(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <p>Total Posts: {posts.length}</p>
+    <ul>
+      {posts.map(post => (
+        
+        <li key={post.id}>ID: {post.id} <br></br>Title: {post.title}<br></br>Description: {post.body}<br></br><br></br></li>
+      ))}
+    </ul>
     </div>
   );
 }
